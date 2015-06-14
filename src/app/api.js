3745848b52a9e2290abbeb080a04a3ce97bab986
Uri Acuesta <acuestal@hhmi.org>
2015-06-14 10:00:47 -0400

    added fetch

---
+++ src/app/api.js
@@ -0,0 +1,17 @@
+import 'fetch'; // Run the Global Variable
+
+class api {
+    getDota() {
+        return fetch('http://www.reddit.com/r/dota2.json')
+            .then(function(response) {
+                return response.text();
+            })
+            .then(function(body) {
+                return JSON.parse(body)
+                    .data.children
+                    .map(x => x.data.title);
+            });
+    }
+}
+
+export default api;
